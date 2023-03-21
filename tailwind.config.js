3d39b05f762d778e19a706aa075bcdba5c0a6529
Chase Vaitses <cvaitses@gmail.com>
2023-03-21 17:32:29 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'seafoam': '#bfd5ca',
+        'seafoamLight': '#d2e0d9',
+        'orange': '#e8891d',
+        'oceanblue': '#006373',
+        'burntorange': '#b35a22',
+        'teal': '#05a3a4',
+      },
+    },
+  },
+  plugins: [],
+}
